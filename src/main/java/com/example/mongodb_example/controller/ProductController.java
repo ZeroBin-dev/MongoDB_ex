57723034982d4f24958c7bbab8c3a176882d7e3e
CYB <zero-bin@naver.com>
2024-10-22 10:38:30 +0900

    소스 완성

--- src/main/java/com/example/mongodb_example/controller/ProductController.java
+++ src/main/java/com/example/mongodb_example/controller/ProductController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,7 +19,7 @@ public class ProductController {
    * 생성
    */
   @PostMapping
-  public Product createProduct(@RequestBody Product product){
+  public Product createProduct(@RequestBody Product product) {
     return productRepository.save(product);
   }
 
@@ -26,7 +27,7 @@ public class ProductController {
    * 전체조회
    */
   @GetMapping
-  public List<Product> getAllProducts(){
+  public List<Product> getAllProducts() {
     return productRepository.findAll();
   }
 
@@ -34,17 +35,32 @@ public class ProductController {
    * 상세조회
    */
   @GetMapping("/{id}")
-  
-
+  public Optional<Product> getProductById(@PathVariable String id) {
+    return productRepository.findById(id);
+  }
 
   /**
    * 수정
    */
   @PutMapping("/{id}")
+  public Product updateProduct(@PathVariable String id, @RequestBody Product productDetail) {
+    Optional<Product> optionalProduct = productRepository.findById(id);
+    if (optionalProduct.isPresent()) {
+      Product product = optionalProduct.get();
+      product.setName(productDetail.getName());
+      product.setPrice(productDetail.getPrice());
+      return productRepository.save(product);
+    } else {
+      return null;
+    }
+  }
 
   /**
    * 삭제
    */
   @DeleteMapping("/{id}")
+  public void deleteProduct(@PathVariable String id) {
+    productRepository.deleteById(id);
+  }
 
 }
